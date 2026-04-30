07fd952a42e401595f72363e588ac72cd70a822b
mhd7pf <realmoddoss@gmail.com>
2026-04-30 11:33:08 +0200

    first deployment

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: '2rem', textAlign: 'center' }}>
+      <h1>🚀 Mon premier déploiement</h1>
+      <p>Projet réalisé par <strong>Thierno DIALLO</strong></p>
+      <p>Future application : gestion de tâches Kanban</p>
+    </div>
+  );
+}
+
+export default App;
